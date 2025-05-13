103d1d1142a79c75ec4e0f8d2f79265bbf5e30b1
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-13 11:40:19 -0400

    Create Backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
 
+// server.js
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+let votes = {
+    Laptop: 0,
+    Desktop: 0
+};
+
+app.post('/vote', (req, res) => {
+    const { option } = req.body;
+    if (votes[option] !== undefined) {
+        votes[option]++;
+        res.status(200).send({ message: 'Vote counted!' });
+    } else {
+        res.status(400).send({ message: 'Invalid option!' });
+    }
+});
+app.get('/results', (req, res) => {
+    res.json(votes);
+});
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
